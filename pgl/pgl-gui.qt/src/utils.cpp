105e959b06955e159412b6f5923a3bfadfd108f9
freemind <freemind@lavabit.com>
2011-03-14 03:36:07 +0000

    Added more handy functions. One is a dialog to select files. The other is to get a line from a file that contains the specified string. And the last one is to get the string value of a script variable. Useful to get default values from pglcmd.conf.

--- pgl/pgl-gui.qt/src/utils.cpp
+++ pgl/pgl-gui.qt/src/utils.cpp
@@ -1,6 +1,8 @@
 
 #include "utils.h"
 #include <QFile>
+#include <QFileDialog>
+#include <QMap>
 
 QString getValidPath(const QString &path, const QString &defaultPath )
 {
@@ -15,3 +17,65 @@ QString getValidPath(const QString &path, const QString &defaultPath )
     
     return new_path;
 }
+
+QStringList selectFiles(QWidget * parent, QString title, QString startPath)
+{
+    QString filter;
+    filter += "All Suported files  (*.p2p *.gz *.7z *.zip *.dat );;";
+    filter += "Peerguardian P2P format (*.p2p);;zip (*.zip);; 7z (*.7z);;GNU zip (*.gz);;dat (*.dat)";
+    
+     QStringList files = QFileDialog::getOpenFileNames(
+                         parent,
+                         title,
+                         startPath,
+                         filter);
+
+    return files;
+}
+
+QString getVariable(QString& line)
+{
+    if ( line.contains("=") )
+        line = line.split("=", QString::SkipEmptyParts)[1];
+    if ( line.contains('"') )
+        line = line.split('"', QString::SkipEmptyParts)[0];
+    
+    return line;
+}
+
+QString getVariable(const QString& path, const QString& search)
+{
+    QString line = getLineWith(path, search);
+    if ( line.contains("=") )
+        line = line.split("=", QString::SkipEmptyParts)[1];
+    if ( line.contains('"') )
+        line = line.split('"', QString::SkipEmptyParts)[0];
+    
+    return line;
+}
+
+QString getLineWith(const QString& path, const QString& search)
+{	
+    QFile file( path );
+    QString line ("");
+	if ( path.isEmpty() ) {
+		qWarning() << Q_FUNC_INFO << "Empty file path given, doing nothing";
+		return line;
+	}
+    else if ( ! file.open( QIODevice::ReadOnly | QIODevice::Text ) ) {
+		qWarning() << Q_FUNC_INFO << "Could not read from file" << path;
+		return line;
+	}
+    
+    QTextStream in( &file );
+    
+	while ( ! in.atEnd() ) 
+    {
+        line = in.readLine().trimmed();
+        if ( line.contains(search) )
+            return line;
+	}
+    
+    return QString("");
+}
+
--- pgl/pgl-gui.qt/src/utils.h
+++ pgl/pgl-gui.qt/src/utils.h
@@ -1,3 +1,12 @@
 #include <QString> 
+#include <QDir>
+#include <QFile>
+#include <QTextStream>
+#include <QDebug>
 
 QString getValidPath(const QString &path, const QString &defaultPath );
+QStringList selectFiles(QWidget * parent=0, QString title="Select one or more Blocklists", QString startPath=QDir::homePath());
+QString getVariable(QString&);
+QString getVariable(const QString&, const QString&);
+QString getLineWith(const QString&, const QString&);
+
