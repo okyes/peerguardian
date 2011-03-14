44b7dae49120f3011ac7bc0363718817fb59d338
freemind <freemind@lavabit.com>
2011-03-14 04:03:18 +0000

    Class(es) to manage whitelisted ips or ports.

---
+++ pgl/pgl-gui.qt/src/pgl_whitelist.cpp
@@ -0,0 +1,169 @@
+
+#include "pgl_whitelist.h"
+#include "utils.h"
+#include "pgl_settings.h"
+#include "file_transactions.h"
+#include <QDebug>
+
+
+WhitelistItem::WhitelistItem(QString& value, int connection, bool enabled, QString& group)
+{
+    m_Value = value;
+    
+    m_Type = PORT;
+    /*if ( isValidIp(value) )
+        m_Type = IP;
+    else if ( getPort(value)  != -1 )
+        m_Type = PORT;
+    else
+        m_Type = INVALID;*/
+    
+    m_Transport = TCP;
+    m_Connection = connection;
+    m_Group = group;
+    m_Enabled = enabled;
+}
+
+QString WhitelistItem::getConnectionAsString()
+{
+    switch(m_Connection)
+    {
+        case TYPE_INCOMING: return QString("Incoming");
+        case TYPE_OUTGOING: return QString("Outgoing");
+        case TYPE_FORWARD: return QString("Forward"); 
+        default: return QString("");
+    }
+}
+
+QString WhitelistItem::getTypeAsString()
+{
+    switch(m_Connection)
+    {
+        case IP: return QString("IP");
+        case PORT: return QString("Port");
+        default: return QString("");
+    }
+}
+
+PglWhitelist::PglWhitelist(QSettings* settings)
+{
+    m_WhitelistFile = getVariable(PGLCMD_DEFAULTS_PATH, "CONFDIR") + QString("/");
+    m_WhitelistFile += getVariable(PGLCMD_DEFAULTS_PATH, "CMD_NAME") + QString(".conf");
+    
+    m_Settings = settings;
+    
+    if ( m_WhitelistFile.isEmpty() )
+        return;
+        
+    m_WhitelistConnection[WHITE_IP_IN] = TYPE_INCOMING;
+    m_WhitelistConnection[WHITE_IP_OUT] = TYPE_OUTGOING;
+    m_WhitelistConnection[WHITE_IP_FWD] = TYPE_FORWARD;
+    m_WhitelistConnection[WHITE_TCP_IN] = TYPE_INCOMING;
+    m_WhitelistConnection[WHITE_UDP_IN] = TYPE_INCOMING;
+    m_WhitelistConnection[WHITE_TCP_OUT] = TYPE_OUTGOING;
+    m_WhitelistConnection[WHITE_UDP_OUT] = TYPE_OUTGOING;
+    m_WhitelistConnection[WHITE_TCP_FWD] = TYPE_FORWARD;
+    m_WhitelistConnection[WHITE_UDP_FWD] = TYPE_FORWARD;
+    
+    QStringList fileData = getFileData(m_WhitelistFile);
+    
+    foreach(QString line, fileData)
+    {
+        if( line.startsWith("#") )
+            continue;
+        
+        foreach(QString key, m_WhitelistConnection.keys())
+        {
+            if ( line.contains(key) )
+            {
+                QString variable = getVariable(line);
+                m_Whitelist[key] = variable.split(" ", QString::SkipEmptyParts);
+                
+                foreach( QString value, variable.split(" ", QString::SkipEmptyParts))
+                    m_WhitelistedItems << WhitelistItem(value, m_WhitelistConnection[key], true, key);
+            }
+        }
+    }
+    
+    qDebug() << m_Whitelist;
+    
+    //Since disabled whitelisted items (IPs or Ports) can't be easily stored
+    //in /etc/plg/pglcmd.conf, the best option is to store them on the GUI settings file
+    QString disabled_items;
+    
+    foreach ( QString key, m_WhitelistConnection.keys() )
+    {
+        disabled_items = settings->value(QString("whitelist/%1").arg(key)).toString();
+        if ( ! disabled_items.isEmpty() )
+            foreach( QString value, disabled_items.split(" ", QString::SkipEmptyParts) )
+                m_WhitelistedItems << WhitelistItem(value, m_WhitelistConnection[key], false, key);
+    }
+}
+
+QList<WhitelistItem> PglWhitelist::getWhitelistItems(QList<QTreeWidgetItem*> treeItems)
+{
+    QList<WhitelistItem> disabledItems;
+    
+    foreach(QTreeWidgetItem* treeItem, treeItems)
+        foreach(WhitelistItem whiteItem, m_WhitelistedItems)
+            if ( whiteItem.value() == treeItem->text(0) )
+                disabledItems << whiteItem;
+
+    return disabledItems;
+}
+
+void PglWhitelist::disableItems(QList<QTreeWidgetItem*> treeItems)
+{
+    if ( m_WhitelistFile.isEmpty() )    
+        return;
+    
+    QList<WhitelistItem> disabledItems = getWhitelistItems(treeItems);
+    
+    
+    QStringList valuesToDelete;
+    foreach(QTreeWidgetItem* treeItem, treeItems)
+        valuesToDelete << treeItem->text(0);
+    
+    //Remove data from pglcmd.conf
+    QStringList fileData = getFileData(m_WhitelistFile);
+    QStringList newData;
+    QStringList values;
+    
+    foreach(QString key, m_WhitelistConnection.keys())
+    {
+        values = m_Whitelist[key];
+        
+        foreach ( QString valueToDelete, valuesToDelete )
+            if ( values.contains(valueToDelete) )
+                values.removeAll(valueToDelete);
+        
+        m_Whitelist[key] = values;
+    }
+    
+    
+    foreach(QString line, fileData)
+    {
+        if ( line.startsWith("#") || line.isEmpty() )
+        {
+            newData << line;
+            continue;
+        }
+        
+        foreach(QString key, m_Whitelist.keys())
+        {
+            if ( line.contains( key ) )
+                newData << key + "=\"" + m_Whitelist[key].join(" ") + "\""; 
+        }        
+        
+    }
+    
+    //FIXME: Needs sudo rights to write back to /etc/pgl/pglcmd.conf
+    saveFileData(newData, "/tmp/pglcmd.conf");
+    
+    //Add data to peerguardian.ini
+    /*foreach( WhitelistItem, items )
+        if ( ! item.isEnabled() )
+            m_Settings->setValue("whitelist/");*/
+    
+}
+
---
+++ pgl/pgl-gui.qt/src/pgl_whitelist.h
@@ -0,0 +1,91 @@
+#ifndef PGL_WHITELIST_H
+#define PGL_WHITELIST_H
+
+#include <QMultiMap>
+#include <QMap>
+#include <QString>
+#include <QStringList>
+#include <QSettings>
+#include <QList>
+#include <QTreeWidgetItem>
+
+#define WHITE_IP_IN "WHITE_IP_IN"
+#define WHITE_IP_OUT "WHITE_IP_OUT"
+#define WHITE_IP_FWD "WHITE_IP_FWD"
+#define WHITE_TCP_IN "WHITE_TCP_IN"
+#define WHITE_UDP_IN "WHITE_UDP_IN"
+#define WHITE_TCP_OUT "WHITE_TCP_OUT"
+#define WHITE_UDP_OUT "WHITE_UDP_OUT"
+#define WHITE_TCP_FWD "WHITE_TCP_FWD"
+#define WHITE_UDP_FWD "WHITE_UDP_FWD"
+
+
+typedef enum { 
+    TYPE_INCOMING,
+    TYPE_OUTGOING,
+	TYPE_FORWARD };
+
+typedef enum {
+    IP,
+    PORT,
+    INVALID
+};
+
+typedef enum {
+    TCP,
+    UDP
+};
+
+
+class WhitelistItem
+{
+    
+    QString m_Value;
+    int m_Connection; //Incoming, Outgoing or Forward
+    int m_Type; //Ip or Port
+    int m_Transport; //TCP or UDP
+    QString m_Group;
+    bool m_Enabled;
+
+    public:
+        WhitelistItem();
+        WhitelistItem(QString&, int, bool, QString&);
+        ~WhitelistItem(){};
+        QString value() { return m_Value; }
+        int connection() { return m_Connection; }
+        int type() { return m_Type; }
+        int transport() { return m_Transport; }
+        QString group() { return m_Group; }
+        bool isEnabled(){ return m_Enabled; }
+        QString getConnectionAsString();
+        QString getTypeAsString();
+        QStringList getAsStringList(){ return QStringList() << m_Value << getTypeAsString(); }
+        
+};
+
+class PglWhitelist 
+{
+    QList<WhitelistItem> m_WhitelistedItems;
+    QString m_WhitelistFile;
+    QMap<QString, int> m_WhitelistConnection;
+    QSettings * m_Settings;
+    QMap<QString, QStringList> m_Whitelist;
+
+	public:
+		/**
+		 * Constructor. Creates an emtpy PglWhitelist object with no data loaded.
+		 */
+		PglWhitelist(QSettings *);
+		/**
+		 * Destructor.
+		 */
+		~PglWhitelist() { }
+        
+        QList<WhitelistItem> getWhitelistedItems(){ return m_WhitelistedItems; }
+        void loadDisabledItems(QSettings*);
+        void disableItems(QList<QTreeWidgetItem*>);
+        QList<WhitelistItem> getWhitelistItems(QList<QTreeWidgetItem*>);
+        
+};
+
+#endif
