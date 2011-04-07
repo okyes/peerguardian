f9021beb6906adb3a4e001ba4c50b4c0f88aee28
freemind <freemind@lavabit.com>
2011-04-07 03:46:31 +0100

    Added new class GuiOptions to maintain the integrity of the options in the configure tab.

---
+++ pgl/pgl-gui.qt/src/gui_options.cpp
@@ -0,0 +1,98 @@
+#include "gui_options.h"
+
+bool GuiOptions::isChanged()
+{
+    
+    if (startAtBoot != m_Window->getStartAtBootBox()->isChecked() )
+        return true;
+        
+    if (updateBlocklistsAutomatically != m_Window->getAutoListUpdateBox()->isChecked() )
+        return true;
+    
+    if ( updateRadioBtn != getActiveUpdateRadioButton() )
+        return true;
+        
+    if ( listStateChanged(m_Window->getBlocklistTreeWidget()) )
+        return true;
+    
+    if ( listStateChanged(m_Window->getWhitelistTreeWidget()) )
+        return true; 
+    
+    return false;
+}
+
+bool GuiOptions::listStateChanged(QTreeWidget * tree)
+{
+    QList<int> listState;
+    
+    if ( tree->objectName().contains("Blocklist", Qt::CaseInsensitive) )
+        listState = m_BlocklistState;
+    else
+        listState = m_WhitelistState;
+    
+    //test if new items were added
+    if ( tree->topLevelItemCount() != listState.size() )
+        return true;
+        
+    for ( int i=0; i < tree->topLevelItemCount(); i++ )
+    {
+        QTreeWidgetItem * item = tree->topLevelItem(i);
+        
+        if ( item->checkState(0) != listState[i] )
+            return true;
+    }
+    
+    return false;
+}
+
+QString GuiOptions::getActiveUpdateRadioButton()
+{
+    
+    if ( m_Window->getAutoListUpdateDailyRadio()->isChecked() )
+        return m_Window->getAutoListUpdateDailyRadio()->objectName();
+    else if ( m_Window->getAutoListUpdateWeeklyRadio()->isChecked() )
+        return m_Window->getAutoListUpdateWeeklyRadio()->objectName();
+    else if ( m_Window->getAutoListUpdateMonthlyRadio()->isChecked() )
+        return m_Window->getAutoListUpdateMonthlyRadio()->objectName();
+        
+    return QString("");
+}
+
+/********* Update methods ************/
+
+void GuiOptions::updateUpdateRadioBtn()
+{
+    updateBlocklistsAutomatically = m_Window->getAutoListUpdateBox()->isChecked();
+    updateRadioBtn = getActiveUpdateRadioButton();
+}
+
+
+void GuiOptions::updateStartAtBoot()
+{
+    startAtBoot = m_Window->getStartAtBootBox()->isChecked();
+}
+
+void GuiOptions::updateList(QTreeWidget * tree)
+{
+    QList<int> listState;
+    
+    for ( int i=0; i < tree->topLevelItemCount(); i++ )
+    {
+        QTreeWidgetItem * item = tree->topLevelItem(i);
+        listState.append(item->checkState(0));
+    }
+
+    if ( tree->objectName().contains("Blocklist", Qt::CaseInsensitive) )
+        m_BlocklistState = listState;
+    else
+        m_WhitelistState = listState;
+
+}
+
+void GuiOptions::update()
+{
+    updateStartAtBoot();
+    updateUpdateRadioBtn();
+    updateList(m_Window->getBlocklistTreeWidget());
+    updateList(m_Window->getWhitelistTreeWidget());
+}
---
+++ pgl/pgl-gui.qt/src/gui_options.h
@@ -0,0 +1,39 @@
+
+#ifndef GUI_OPTIONS_H
+#define GUI_OPTIONS_H
+
+#include <QList>
+#include <QMainWindow>
+#include <QTreeWidget>
+#include <QTreeWidgetItem>
+#include <QString>
+#include <QDebug>
+#include "peerguardian.h"
+
+class Peerguardian;
+ 
+class GuiOptions
+{
+    Peerguardian * m_Window;
+    QString updateRadioBtn;
+    bool startAtBoot;
+    bool updateBlocklistsAutomatically;
+    QList<int> m_BlocklistState;
+    QList<int> m_WhitelistState;
+    
+    public:
+        GuiOptions() { m_Window = NULL; }
+        GuiOptions(Peerguardian * gui) { m_Window = gui; }
+        ~GuiOptions(){ qDebug() << "~GuiOptions()"; }
+        void addWindow(Peerguardian * gui) { m_Window = gui; }
+        bool isChanged();
+        void updateUpdateRadioBtn();
+        void updateStartAtBoot();
+        void update();
+        QString getActiveUpdateRadioButton();
+        bool listStateChanged(QTreeWidget * tree);
+        void updateList(QTreeWidget * tree);
+};
+
+
+#endif
