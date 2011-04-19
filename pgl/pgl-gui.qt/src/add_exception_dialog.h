91a98dcc2d892bf916adffb8a228f47aeaf8df09
freemind <freemind@lavabit.com>
2011-04-19 03:09:46 +0100

    Rewritten many code to fix the bug of alias (http <-> 80) and to verify if a certain protocol runs TCP and/or UDP. Cleaned the code part that managed the errors.

--- pgl/pgl-gui.qt/src/add_exception_dialog.h
+++ pgl/pgl-gui.qt/src/add_exception_dialog.h
@@ -18,30 +18,55 @@
 #include <QList>
 #include <QNetworkAccessManager>
 #include <QNetworkReply>
+#include <QHash>
 
 #include "ui_add_exception.h"
 #include "pgl_whitelist.h"
+
+
 
 typedef enum openMode { ADD_MODE=0x000,
                 EDIT_MODE,
                 BLOCKLIST_MODE,
                 EXCEPTION_MODE
             };
+
+
+/*class Port
+{
+    QString m_desig;
+    int m_number;
+    QString m_protocol;
+    
+    public:
+        Port(){};
+        Port(QString desig, QString prot, int n=0) { m_desig = desig; m_protocol = prot; m_number = n; }
+        Port(int n) { m_number = n; }
+        ~Port(){};
         
+        int number(){ return m_number;}
+        QString desig(){ return m_desig; }
+        QString protocol() { return m_protocol; }
+        void addAlias(const QString& alias) {}
+};*/
             
 
 class AddExceptionDialog : public QDialog, private Ui::AddExceptionDialog {
 
 	Q_OBJECT
     
-    QMap<QString, int> ports;
-    QList<WhitelistItem> m_NewItems;
+    QHash<QString, int> ports;
+    QList<Port> m_ports;
     QList<WhitelistItem> m_Items;
+    QList<WhitelistItem> m_validItems;
+    QList<WhitelistItem> m_invalidItems;
+    QStringList reasons;
     int mode;
     QStringList m_validExtensions;
     QNetworkAccessManager *m_manager;
     QList<QUrl> m_urls;
     QStringList m_blocklists;
+    
 	
 	public:
         AddExceptionDialog(QWidget *p = 0, int mode=0);
@@ -53,15 +78,16 @@ class AddExceptionDialog : public QDialog, private Ui::AddExceptionDialog {
         QVector<QTreeWidgetItem*> getTreeItems(QTreeWidget *);
         bool isValidBlocklist(QString&);
         bool isValidException(QString&);
-        QList<WhitelistItem> getItems(){ return m_NewItems; }
+        QList<WhitelistItem> getItems(){ return m_validItems; }
         QStringList getBlocklists() { return m_blocklists; }
-        bool isNew(WhitelistItem& whiteItem);
-        QList<WhitelistItem> getWhitelistItems(QString&, bool);
+        bool isValidWhitelistItem(WhitelistItem& whiteItem, QString& reason);
+        void setWhitelistItems(QString&, bool);
         QStringList getConnections();
         QStringList getProtocols(bool);
-        QStringList getParams(QString& text);
+        QStringList getParams(const QString& text);
         void setPortsFromFile();
         bool isPort(QString &);
+        Port getPortFromLine(QString);
         
     public slots:
         void addEntry();
