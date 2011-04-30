cfb9ba0f6790c002c854299a34c760e0f57563a3
jre <jre-phoenix@users.sourceforge.net>
2011-04-30 09:41:37 +0200

    reenabled PLUGINDIR for dbus.so
    basic testing is over. We need "make install" to work again.

--- pgl/pgld/src/pgld.c
+++ pgl/pgld/src/pgld.c
@@ -119,8 +119,7 @@ static int open_dbus() {
 
     char *err;
 
-//    dbus_lh = dlopen(PLUGINDIR "/dbus.so", RTLD_NOW);
-    dbus_lh = dlopen( "./dbus.so", RTLD_NOW); //FIXME: temporary fix for testing purposes, will not work for make install
+    dbus_lh = dlopen(PLUGINDIR "/dbus.so", RTLD_NOW);
     if (!dbus_lh) {
         do_log(LOG_ERR, "ERROR: dlopen() failed: %s", dlerror());
         return -1;
