232d1d5fc50a5bfbcd7c29c9d36ce07dfb97d3f5
jre <jre-phoenix@users.sourceforge.net>
2011-08-08 17:16:21 +0200

    declare function 'do_log_xdbus'

--- pgl/pgld/src/pgld.h
+++ pgl/pgld/src/pgld.h
@@ -77,6 +77,7 @@
     } while(0);                                             \
 
 void do_log(int priority, const char *format, ...);
+void do_log_xdbus(int priority, const char *format, ...);
 void int2ip (uint32_t ipint, char *ipstr);
 
 #endif /* INC_PGLD_H */
