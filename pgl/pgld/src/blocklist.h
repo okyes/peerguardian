d66302ccf6664b79a33eaab52f89d54c4138ee5e
jre <jre-phoenix@users.sourceforge.net>
2011-08-09 01:46:14 +0200

    relicensed pgld and pglcmd to GPL v3 (or later)
    fixed headers
    added/updated copyright notices in the files and in the applications'
    output
    mention 3rd party in docs/AUTHORS

--- pgl/pgld/src/blocklist.h
+++ pgl/pgld/src/blocklist.h
@@ -1,25 +1,22 @@
-/*
-   Blocklist management
+// pgld - blocklist management
+//
+// Copyright (C) 2008 Jindrich Makovicka (makovick@gmail.com)
+//
+// This file is part of pgl.
+//
+// pgl is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// pgl is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-   (c) 2008 Jindrich Makovicka (makovick@gmail.com)
-
-   This file is part of pgl.
-
-   pgld is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
-
-   pgld is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with GNU Emacs; see the file COPYING.  If not, write to
-   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.
-*/
 
 #ifndef INC_BLOCKLIST_H
 #define INC_BLOCKLIST_H
--- pgl/pgld/src/pgld.c
+++ pgl/pgld/src/pgld.c
@@ -1,27 +1,23 @@
-/*
-   Netfilter blocking daemon
+// pgld - netfilter blocking daemon
+//
+// Copyright (C) 2008 Jindrich Makovicka (makovick@gmail.com)
+// Copyright (C) 2004 Morpheus (ebutera@users.berlios.de)
+//
+// This file is part of pgl.
+//
+// pgl is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// pgl is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-   (c) 2008 Jindrich Makovicka (makovick@gmail.com)
-
-   Portions (c) 2004 Morpheus (ebutera@users.berlios.de)
-
-   This file is part of pgl.
-
-   pgl is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
-
-   pgl is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with GNU Emacs; see the file COPYING.  If not, write to
-   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.
-*/
 
 #include "pgld.h"
 
@@ -578,6 +574,9 @@ static void nfqueue_loop () {
 
 static void print_usage() {
     fprintf(stderr, PACKAGE_NAME " " VERSION "\n\n");
+    fprintf(stderr, "pgl is licensed under the GNU General Public License v3, or (at your option)\n");
+    fprintf(stderr, "any later version. This program comes with ABSOLUTELY NO WARRANTY. This is\n");
+    fprintf(stderr, "free software, and you are welcome to modify and/or redistribute it.\n\n");
     fprintf(stderr, "Usage:\n");
     fprintf(stderr, "  pgld [-s] [-l LOGFILE]");
 #ifdef HAVE_DBUS
