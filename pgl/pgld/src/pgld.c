okyes/peerguardian

commit a9978a6c8b3e53c1815500de22d321b71eed6eb5

Removed the (non-default) feature to directly DROP and ACCEPT packets
removed the (non-default) feature to directly DROP and ACCEPT packets
(as in MoBlock 0.8), instead of using the (default) MARKing feature.

Reason:
ACCEPTing/DROPing packets directly was only needed for kernel/netfilter
not supporting MARKing. Probably it was also a bit more efficient than
MARKing.
But ACCEPTing packets directly breaks other iptables setups - this has
always been documented, but wasn't known to all users. So in order to
avoid misconfigurations I decided to drop this feature.
DROPing directly should not yield any drawbacks, but I decided to also
remove it in order to make the code simpler.

pgl/pgld/src/pgld.c
@@ -400,13 +400,9 @@ static int nfqueue_cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg, struct nf
         case NF_IP_LOCAL_IN:
             found_range = blocklist_find(ntohl(ip->saddr));
             if (found_range) {
-                if (reject_mark) {
-                    // we set the user-defined reject_mark and set NF_REPEAT verdict
-                    // it's up to other iptables rules to decide what to do with this marked packet
-                    nfq_set_verdict2(qh, id, NF_REPEAT, reject_mark, 0, NULL);
-                } else {
-                    status = nfq_set_verdict(qh, id, NF_DROP, 0, NULL);
-                }
+                // we set the user-defined reject_mark and set NF_REPEAT verdict
+                // it's up to other iptables rules to decide what to do with this marked packet
+                nfq_set_verdict2(qh, id, NF_REPEAT, reject_mark, 0, NULL);
                 found_range->hits++;
                 // NOTE: "setipinfo" sets the formats.
                 // TODO: Separate IP and port in there.
@@ -416,26 +412,19 @@ static int nfqueue_cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg, struct nf
 #else
                 do_log(LOG_NOTICE, " IN: %-22s %-22s %-4s",src,dst,proto);
 #endif
-            } else if (accept_mark) {
+            } else {
                 // we set the user-defined accept_mark and set NF_REPEAT verdict
                 // it's up to other iptables rules to decide what to do with this marked packet
                 nfq_set_verdict2(qh, id, NF_REPEAT, accept_mark, 0, NULL);
-            } else {
-                // no accept_mark, just NF_ACCEPT the packet
-                status = nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL);
             }
             break;
         case NF_IP_LOCAL_OUT:
 
             found_range = blocklist_find(ntohl(ip->daddr));
             if (found_range) {
-                if (reject_mark) {
-                    // we set the user-defined reject_mark and set NF_REPEAT verdict
-                    // it's up to other iptables rules to decide what to do with this marked packet
-                    nfq_set_verdict2(qh, id, NF_REPEAT, reject_mark, 0, NULL);
-                } else {
-                    status = nfq_set_verdict(qh, id, NF_DROP, 0, NULL);
-                }
+                // we set the user-defined reject_mark and set NF_REPEAT verdict
+                // it's up to other iptables rules to decide what to do with this marked packet
+                nfq_set_verdict2(qh, id, NF_REPEAT, reject_mark, 0, NULL);
                 found_range->hits++;
                 // NOTE: "setipinfo" sets the formats.
                 // TODO: Separate IP and port in there.
@@ -457,13 +446,10 @@ static int nfqueue_cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg, struct nf
 #else
                 do_log(LOG_NOTICE, "OUT: %-22s %-22s %-4su", src,dst,proto);
 #endif
-            } else if (accept_mark) {
+            } else {
                 // we set the user-defined accept_mark and set NF_REPEAT verdict
                 // it's up to other iptables rules to decide what to do with this marked packet
                 nfq_set_verdict2(qh, id, NF_REPEAT, accept_mark, 0, NULL);
-            } else {
-                // no accept_mark, just NF_ACCEPT the packet
-                status = nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL);
             }
             break;
         case NF_IP_FORWARD:
@@ -472,14 +458,9 @@ static int nfqueue_cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg, struct nf
                 found_range = blocklist_find(ntohl(ip->daddr));
             }
             if (found_range) {
-                if (reject_mark) {
-                    // we set the user-defined reject_mark and set NF_REPEAT verdict
-                    // it's up to other iptables rules to decide what to do with this marked packet
-                    nfq_set_verdict2(qh, id, NF_REPEAT, reject_mark, 0, NULL);
-                } else {
-                    status = nfq_set_verdict(qh, id, NF_DROP, 0, NULL);
-                }
-
+                // we set the user-defined reject_mark and set NF_REPEAT verdict
+                // it's up to other iptables rules to decide what to do with this marked packet
+                nfq_set_verdict2(qh, id, NF_REPEAT, reject_mark, 0, NULL);
                 found_range->hits++;
                 setipinfo(src, dst, proto, ip, payload);
 #ifndef LOWMEM
@@ -487,13 +468,10 @@ static int nfqueue_cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg, struct nf
 #else
                 do_log(LOG_NOTICE, "FWD: %-22s %-22s %-4s",src,dst,proto);
 #endif
-            } else if (accept_mark) {
+            } else {
                 // we set the user-defined accept_mark and set NF_REPEAT verdict
                 // it's up to other iptables rules to decide what to do with this marked packet
                 nfq_set_verdict2(qh, id, NF_REPEAT, accept_mark, 0, NULL);
-            } else {
-                // no accept_mark, just NF_ACCEPT the packet
-                status = nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL);
             }
             break;
         default:
@@ -589,7 +567,7 @@ static void print_usage() {
 #ifdef HAVE_DBUS
     fprintf(stderr, " [-d]");
 #endif
-    fprintf(stderr, " [-c CHARSET] [-p PIDFILE] [-a MARK] [-r MARK] [-q 0-65535] BLOCKLIST(S)\n");
+    fprintf(stderr, " [-c CHARSET] [-p PIDFILE] -a MARK -r MARK [-q 0-65535] BLOCKLIST(S)\n");
     fprintf(stderr, "  pgld [-c CHARSET] -m [BLOCKLIST(S)]\n\n");
     fprintf(stderr, "Options:\n");
     fprintf(stderr, "  -s                Enable logging to syslog.\n");

pgl/README
@@ -43,10 +43,8 @@ feature.
 WARNING: Users with firewall (iptables rules)
 
 pgl does not conflict with other firewalls (iptables rules) as long as you make
-sure the following three conditions hold:
-- pgl marks non-matched (IP is not in the blocklist) packets. (The marking
-  feature is on per default in pglcmd.)
-- Other firewalls must not mark packets.
+sure the following two conditions hold:
+- Other firewalls must not MARK packets.
 - pgl is started after other firewalls. If other firewalls are
   started/reloaded after pgl, then you may need to restart pgl again. You will
   be fine, if the iptables rules which send traffic to the iptables chains
@@ -62,14 +60,10 @@ pgld checks traffic (packets) that is sent to the iptables NFQUEUE target. If
 the necessary netfilter support is not built in the kernel directly, pglcmd will
 load the necessary kernel modules. If they are not available, pgld can not be
 started.
-If a packet matches the blocklist, then pgld DROPs it directly. If configured
-pgld MARKs the packet instead. Per default the MARKing feature is on if you use
-pglcmd. So blocked packets get the MARK "10", which is shown as "0xa" by
-iptables.
-If a packet doesn't match the blocklist, then pgld ACCEPTs it directly. If
-configured pgld MARKs the packet instead. Per default the MARKing feature is on
-if you use pglcmd. So allowed packets get the MARK "20", which is shown as
-"0x14" by iptables.
+If a packet matches the blocklist, then pgld MARKs the packet. Blocked packets
+get the MARK "10", which is shown as "0xa" by iptables.
+If a packet doesn't match the blocklist, then pgld MARKs the packet. Allowed
+packets get the MARK "20", which is shown as "0x14" by iptables.
 A MARKed packet repeats the hook function (NF_REPEAT). So it is sent back to
 the head of the iptables chain again. This means it enters the chain INPUT,
 OUTPUT or FORWARD again, but this time bearing a MARK. Then further iptables
@@ -78,8 +72,6 @@ Per default pglcmd sets iptables rules to REJECT outgoing packets, and to DROP
 incoming and forwarded packets, if they were "marked block". If they were
 "marked allow", pglcmd's iptables setup just ignores them, so other iptables
 rules decide what happens to them.
-It is strongly recommended to use the MARKing feature, because this allows to
-integrate pgl with other firewalls.
 A packet may only bear one mark, so there mustn't be any other applications or
 iptables rules that mark packets. Otherwise the setup will not work and the
 packet will loop forever.

pgl/docs/pgld.1
@@ -25,7 +25,7 @@
 .SH SYNOPSIS
 .B pgld
 [\fI\-s\fR] [\fI\-l LOGFILE\fR] [\fI\-d\fR] [\fI\-c CHARSET\fR] [\fI\-p
-PIDFILE\fR] [\fI\-q 0-65535\fR] [\fI\-r MARK\fR] [\fI\-a MARK\fR] BLOCKLIST(S)
+PIDFILE\fR] [\fI\-q 0-65535\fR] \fI\-r MARK\fR \fI\-a MARK\fR BLOCKLIST(S)
 .PP
 .B pgld
 [\fI\-c CHARSET\fR] \-m [\fIBLOCKLIST(S)\fR]
@@ -277,15 +277,11 @@ the necessary netfilter support is not built in the kernel directly, pglcmd will
 load the necessary kernel modules. If they are not available, pgld can not be
 started.
 .PP
-If a packet matches the blocklist, then pgld DROPs it directly. If configured
-pgld MARKs the packet instead. Per default the MARKing feature is on if you use
-pglcmd. So blocked packets get the MARK "10", which is shown as "0xa" by
-iptables.
+If a packet matches the blocklist, then pgld MARKs the packet. Blocked packets
+get the MARK "10", which is shown as "0xa" by iptables.
 .PP
-If a packet doesn't match the blocklist, then pgld ACCEPTs it directly. If
-configured pgld MARKs the packet instead. Per default the MARKing feature is on
-if you use pglcmd. So allowed packets get the MARK "20", which is shown as
-"0x14" by iptables.
+If a packet doesn't match the blocklist, then pgld MARKs the packet. Allowed
+packets get the MARK "20", which is shown as "0x14" by iptables.
 .PP
 A MARKed packet repeats the hook function (NF_REPEAT). So it is sent back to
 the head of the iptables chain again. This means it enters the chain INPUT,
@@ -307,12 +303,9 @@ packet will loop forever.
 
 .SH WARNING: Users with firewall (iptables rules)
 \fBpgl\fR does not conflict with other firewalls (iptables rules) as long as you
-make sure the following three conditions hold:
+make sure the following two conditions hold:
 .IP \(em 3
-pgl marks non-matched (IP is not in the blocklist) packets. (The marking feature
-is on per default in pglcmd.)
-.IP \(em 3
-Other firewalls must not mark packets.
+Other firewalls must not MARK packets.
 .IP \(em 3
 pgl is started after other firewalls. If other firewalls are started or reloaded
 after pgl, then you may need to restart pglcmd again. You will be fine, if the

pgl/pglcmd/pglcmd.defaults.in
@@ -223,19 +223,15 @@ IPTABLES_TARGET="NFQUEUE"
 # Valid queue numbers are 0 to 65535.
 NFQUEUE_NUMBER="92"
 
-# Set what happens to matched packets (IP is in the blocklist).
-# 0 - DROP them directly (as in MoBlock 0.8).
-# 1 - MARK them. Further iptables rules decide what happens to them. E.g. this
-#     allows to REJECT packets to avoid the long timeout, which occurs when
-#     packets are DROPped, see below. This setting is also necessary for
-#     iptables logging to syslog, see below.
-REJECT="1"
-
-# Set the corresponding MARK
+# Set the iptables MARK that is assigned to matched packets (IP is in the
+# blocklist).
+# MARKed packets repeat the complete hook function. Further iptables rules
+# decide what happens to them. E.g. this allows to REJECT outgoing packets to
+# avoid the long timeout, which occurs when packets are DROPped, see below.
 REJECT_MARK="10"
 
-# Set the iptables target for "marked block" packets.
-# This section works only for IPTABLES_ACTIVATION="1" and REJECT="1".
+# Set the iptables target for matched packets (IP is in the blocklist).
+# This section works only for IPTABLES_ACTIVATION="1".
 # Valid values are all iptables targets. Be careful: senseless values are also
 # accepted.
 # REJECT: The sender of the packet is notified that the packet was blocked.
@@ -244,13 +240,10 @@ REJECT_IN="DROP"
 REJECT_OUT="REJECT"
 REJECT_FWD="DROP"
 
-# Set what happens to non-matched packets (IP is not in the blocklist).
-# 0 - ACCEPT them directly (as in MoBlock 0.8)
-# 1 - MARK them. pgl will then ignore them. This allows an integration with
-#     other firewalls.
-ACCEPT="1"
-
-# Set the corresponding MARK
+# Set the iptables MARK that is assigned to non-matched packets (IP is not in
+# the blocklist).
+# MARKed packets repeat the complete hook function. pgl ignores packets with
+# this MARK. This allows an integration with other firewalls.
 ACCEPT_MARK="20"
 
 # Set the iptables target for whitelisting packets.
@@ -353,8 +346,7 @@ LOG_SYSLOG="0"
 LOG_LOGFILE="1"
 
 # Iptables logging of blocked packets
-# Set an iptables target for blocked packets. This will only work if marking
-# matched (IP is in the blocklist) packets is on (i.e. REJECT="1").
+# Set an iptables target for blocked packets.
 # The iptables rules will be inserted directly before the iptables rule which
 # decides what happens to "marked block" packets.
 # Examples:

pgl/pglcmd/pglcmd.lib
@@ -614,40 +614,26 @@ test_IPTABLES_TARGET () {
     esac
 }
 
-test_REJECT () {
-    case "$REJECT" in
-        '0')
-            true
-            ;;
-        '1')
-            # No test for REJECT_MARK. Valid: decimal and hexadecimal!?
-            # No test for REJECT_IN. Valid: all iptables targets
-            # No test for REJECT_OUT. Valid: all iptables targets
-            # No test for REJECT_FWD. Valid: all iptables targets
-            true
-            ;;
-        *)
-            log_failure_msg "Error $E_CONFIG: Check the REJECT setting."
-            exit $E_CONFIG
-            ;;
-    esac
+# TODO (remove some day): Transition 2014-01-30
+test_ACCEPT () {
+    if [ -n "$ACCEPT" ] && [ "$ACCEPT" = 0 ] ; then
+        log_failure_msg "Error $E_CONFIG: You have set ACCEPT=0. This setting isn't supported anymore. Aborting ..."
+        exit $E_CONFIG
+    fi
 }
 
-test_ACCEPT () {
-    case "$ACCEPT" in
-        '0')
-            true
-            ;;
-        '1')
-            # No test for ACCEPT_MARK. Valid: decimal and hexadecimal!?
-            true
-            ;;
-        *)
-            log_failure_msg "Error $E_CONFIG: Check the ACCEPT setting."
-            exit $E_CONFIG
-            ;;
-    esac
+# TODO (remove some day): Transition 2014-01-30
+test_REJECT () {
+    if [ -n "$REJECT" ] && [ "$REJECT" = 0 ] ; then
+        log_failure_msg "Error $E_CONFIG: You have set REJECT=0. This setting isn't supported anymore. Aborting ..."
+        exit $E_CONFIG
+    fi
 }
+
+# No test for REJECT_MARK. Valid: decimal and hexadecimal!?
+# No test for REJECT_IN. Valid: all iptables targets
+# No test for REJECT_OUT. Valid: all iptables targets
+# No test for REJECT_FWD. Valid: all iptables targets
 
 test_BLOCKLISTS_DIR () {
     [ -d "$BLOCKLISTS_DIR" ] || {
@@ -732,7 +718,6 @@ test_IPTABLES_VARS () {
             #   network IP address (with /mask), or a plain IP address.
             # No test for WHITE_IP_OUT.
             # No test for WHITE_IP_FWD.
-
             # No test for INTERFACES.
             # No test for IP_REMOVE. Valid: all regular expressions for grep
             ;;
@@ -1191,18 +1176,16 @@ insert_iptables () {
             # INPUT/OUTPUT/FORWARD chains, but I decided against that in order to
             # keep those chains cleaner. It has to be made sure that the MARK of the
             # packets is not changed by other iptables rules.
-            if [ "$REJECT" -eq 1 ] ; then
-                echo -n "....Inserting block rule"
-                iptables -I $PGL_CHAIN -m mark --mark $REJECT_MARK -j $REJECT_TARGET || fail_insert_iptables
-                # Log "Marked block" packages: The log-iptables-rules should be
-                # placed directly before the block-iptables-rules (i.e. Insert them
-                # directly after them)
-                if [ -n "$LOG_IPTABLES" ] ; then    # string is not empty
-                    echo -n " and log rule"
-                    iptables -I $PGL_CHAIN -m mark --mark $REJECT_MARK -j $LOG_IPTABLES || fail_insert_iptables
-                fi
-                log_end_msg "$?"
+            echo -n "....Inserting block rule"
+            iptables -I $PGL_CHAIN -m mark --mark $REJECT_MARK -j $REJECT_TARGET || fail_insert_iptables
+            # Log "Marked block" packages: The log-iptables-rules should be
+            # placed directly before the block-iptables-rules (i.e. Insert them
+            # directly after them)
+            if [ -n "$LOG_IPTABLES" ] ; then    # string is not empty
+                echo -n " and log rule"
+                iptables -I $PGL_CHAIN -m mark --mark $REJECT_MARK -j $LOG_IPTABLES || fail_insert_iptables
             fi
+            log_end_msg "$?"
         done
 
         # Automatically whitelist LAN of all up interfaces, loopback interface
@@ -1228,6 +1211,7 @@ insert_iptables () {
             ;;
         '1'|'2')
             echo "..Activating chains:"
+            # Don't send RELATED or ESTABLISHED packets to the pglcmd_* chains.
             case "$IPTABLES_ACTIVATION" in
             '1')
                 CMD_STATE="-m conntrack ! --ctstate RELATED,ESTABLISHED"
@@ -1236,19 +1220,12 @@ insert_iptables () {
                 CMD_STATE=""
                 ;;
             esac
-            case "$ACCEPT" in
-            '0')
-                CMD_MARK_ACCEPT=""
-                ;;
-            '1')
-                # Send all NEW packets, except the "marked accept" packets, to the pglcmd_* chains.
-                # From there "marked" packets are sent back to the head of the chain again
-                # (NF_REPEAT repeats the hook function). Thus "marked accept" packets traverse to
-                # the rest of the iptables setup.
-                # Problem: If another rule changes the MARK the packet will loop forever.
-                CMD_MARK_ACCEPT="-m mark ! --mark $ACCEPT_MARK"
-                ;;
-            esac
+            # Don't send "marked accept" packets to the pglcmd_* chains.
+            # From there "marked" packets are sent back to the head of the chain again
+            # (NF_REPEAT repeats the hook function). Thus "marked accept" packets traverse to
+            # the rest of the iptables setup.
+            # Problem: If another rule changes the MARK the packet will loop forever.
+            CMD_MARK_ACCEPT="-m mark ! --mark $ACCEPT_MARK"
 
             # If INTERFACES is empty/not set, then set it to "all".
             INTERFACES=${INTERFACES:-all}
@@ -1508,8 +1485,7 @@ do_start () {
                 modprobe -q ipt_mark ||
                 if [ "$REJECT" = 1 ] || [ "$ACCEPT" = 1 ] ; then
                     log_failure_msg "Error $E_XEXTERNAL: Could not load kernel module xt_mark, not starting $NAME!"
-                    log_failure_msg "Use a kernel with netfilter MARK support or reconfigure $(basename $0)"
-                    log_failure_msg "to not use the MARKing feature (options REJECT and ACCEPT)."
+                    log_failure_msg "Use a kernel with netfilter MARK support."
                     exit $E_XEXTERNAL
                 else
                     log_warning_msg "Warning: Could not load kernel module xt_mark, continuing anyway."
@@ -1605,18 +1581,10 @@ do_start () {
             fi
 
             # Set the options for marking matched packets
-            if [ "$REJECT" -eq 1 ] ; then
-                REJECT_OPT="-r $REJECT_MARK"
-            else
-                REJECT_OPT=""
-            fi
+            REJECT_OPT="-r $REJECT_MARK"
 
             # Set the options for marking not matched packets
-            if [ "$ACCEPT" -eq 1 ] ; then
-                ACCEPT_OPT="-a $ACCEPT_MARK"
-            else
-                ACCEPT_OPT=""
-            fi
+            ACCEPT_OPT="-a $ACCEPT_MARK"
 
             # Set the NFQUEUE number only if NFQUEUE and not QUEUE is used
             case "$IPTABLES_TARGET" in
@@ -2313,12 +2281,9 @@ test_ipblocking () {
     echo "CAUTION: This is just a simple test to check if $DESC blocks"
     echo "outgoing connections. For this, an IP from the blocklist will be pinged. Then "
     echo "the test checks if this IP appears in $TEST_LOG."
-    if [ "$REJECT" -eq 1 ] ; then
-        echo "$NAME marks packets to be blocked. This means you have to make sure that the"
-        echo "marked packets are also blocked later (with appropriate iptables rules). If you"
-        echo "are using the default configuration and $NAME is started after other firewalls"
-        echo "this will be the case."
-    fi
+    echo "$NAME MARKs packets to be blocked. This means you have to make sure that the"
+    echo "MARKed packets are also blocked later (with appropriate iptables rules)."
+    echo "To do that you can use LOG_IPTABLES=\"LOG --log-level info\""
     echo "This test does not check if you have sane iptables rules. Therefore success"
     echo "doesn't imply that everything is working as you expect it."
     echo ""
@@ -2348,32 +2313,19 @@ test_ipblocking () {
 
     if [ "$RETVAL" -eq 0 ] ; then    # IP in logfile
         if [ "$RETVAL_PING" -eq 1 ] ; then    # No reply to ping
-            if [ "$REJECT" -eq 1 ] ; then
-                echo "$NAME marked the IP to be blocked and the IP did not answer."
-                echo "Test succeeded."
-            else
-                echo "$NAME blocked the IP. Test succeeded."
-            fi
+            echo "$NAME marked the IP to be blocked and the IP did not answer."
+            echo "Test succeeded."
         elif [ "$RETVAL_PING" -eq 0 ] ; then    # Reply to ping
-            if [ "$REJECT" -eq 1 ] ; then
-                echo "$NAME blocked the IP, but ping got an answer or failed."
-                echo "Check the iptables settings and make sure that \"marked block\" packets really"
-                echo "get blocked."
-                RETVAL=2
-            else    # REJECT is 0
-                echo "$NAME blocked the IP, but ping got an answer or failed - strange."
-                RETVAL=2
-            fi
+            echo "$NAME blocked the IP, but ping got an answer or failed."
+            echo "Check the iptables settings and make sure that \"marked block\" packets really"
+            echo "get blocked."
+            RETVAL=2
         else
             echo "Some error occured with ping, no test result."
             RETVAL=2
         fi
     elif [ "$RETVAL" -eq 1 ] ; then    # IP not in logfile
-        if [ "$REJECT" -eq 1 ] ; then
-            echo "$NAME did not mark the IP to be blocked."
-        else    # REJECT is 0
-            echo "$NAME did not block the IP."
-        fi
+        echo "$NAME did not mark the IP to be blocked."
         if [ "$RETVAL_PING" -eq 0 ] ; then    # Reply to ping
             echo "$TEST_IP answered. Test failed."
         elif [ "$RETVAL_PING" -eq 1 ] ; then    # No reply to ping

commit b3b85f2c4bb522ade2606b99ac105481bc26e9ee

Use deprecated nfq_set_verdict_mark instead of nfq_set_verdict2
Revert "Update nfq_set_verdict_mark calls to nfq_set_verdict2"
This reverts commit b5bbde83530f948d787bf4a9b769cd89e15c42f1.
Handled conflicts in pgl/pgld/src/pgld.c caused by commit
a9978a6c8b3e53c1815500de22d321b71eed6eb5
"Removed the (non-default) feature to directly DROP and ACCEPT packets"

debian/control: remove verswioned dependency on libnetfilter-queue

Necessary for Debian Wheezy and Ubuntu Lucid (10.04 LTS),
Precise (12.04 LTS) and Raring (13.04)

pgl/debian/control
@@ -6,7 +6,7 @@ Uploaders: jre <jre-phoenix@users.sourceforge.net>
 Bugs: mailto:peerguardian-devel@lists.sourceforge.net
 Build-Depends: po-debconf,
                libdbus-1-dev,
-               libnetfilter-queue-dev (>= 1.0~),
+               libnetfilter-queue-dev,
                libnfnetlink-dev,
                libpolkit-qt-1-dev,
                libqt4-dev,

pgl/pgld/src/pgld.c
@@ -379,8 +379,7 @@ static int nfqueue_cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg, struct nf
     struct nfqnl_msg_packet_hdr *ph;
     block_entry_t *found_range;
     struct iphdr *ip;
-    unsigned char *payload;
-    char proto[5], src[23], dst[23];  //src and dst are 23 for IP(16)+port(5) + : + NULL
+    char *payload, proto[5], src[23], dst[23];  //src and dst are 23 for IP(16)+port(5) + : + NULL
 
     ph = nfq_get_msg_packet_hdr(nfa);
     if (ph) {
@@ -393,7 +392,7 @@ static int nfqueue_cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg, struct nf
             if (found_range) {
                 // we set the user-defined reject_mark and set NF_REPEAT verdict
                 // it's up to other iptables rules to decide what to do with this marked packet
-                nfq_set_verdict2(qh, id, NF_REPEAT, reject_mark, 0, NULL);
+                status = nfq_set_verdict_mark(qh, id, NF_REPEAT, reject_mark, 0, NULL);
                 found_range->hits++;
                 // NOTE: "setipinfo" sets the formats.
                 // TODO: Separate IP and port in there.
@@ -406,7 +405,7 @@ static int nfqueue_cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg, struct nf
             } else {
                 // we set the user-defined accept_mark and set NF_REPEAT verdict
                 // it's up to other iptables rules to decide what to do with this marked packet
-                nfq_set_verdict2(qh, id, NF_REPEAT, accept_mark, 0, NULL);
+                status = nfq_set_verdict_mark(qh, id, NF_REPEAT, accept_mark, 0, NULL);
             }
             break;
         case NF_IP_LOCAL_OUT:
@@ -415,7 +414,7 @@ static int nfqueue_cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg, struct nf
             if (found_range) {
                 // we set the user-defined reject_mark and set NF_REPEAT verdict
                 // it's up to other iptables rules to decide what to do with this marked packet
-                nfq_set_verdict2(qh, id, NF_REPEAT, reject_mark, 0, NULL);
+                status = nfq_set_verdict_mark(qh, id, NF_REPEAT, reject_mark, 0, NULL);
                 found_range->hits++;
                 // NOTE: "setipinfo" sets the formats.
                 // TODO: Separate IP and port in there.
@@ -440,7 +439,7 @@ static int nfqueue_cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg, struct nf
             } else {
                 // we set the user-defined accept_mark and set NF_REPEAT verdict
                 // it's up to other iptables rules to decide what to do with this marked packet
-                nfq_set_verdict2(qh, id, NF_REPEAT, accept_mark, 0, NULL);
+                status = nfq_set_verdict_mark(qh, id, NF_REPEAT, accept_mark, 0, NULL);
             }
             break;
         case NF_IP_FORWARD:
@@ -451,7 +450,7 @@ static int nfqueue_cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg, struct nf
             if (found_range) {
                 // we set the user-defined reject_mark and set NF_REPEAT verdict
                 // it's up to other iptables rules to decide what to do with this marked packet
-                nfq_set_verdict2(qh, id, NF_REPEAT, reject_mark, 0, NULL);
+                status = nfq_set_verdict_mark(qh, id, NF_REPEAT, reject_mark, 0, NULL);
                 found_range->hits++;
                 setipinfo(src, dst, proto, ip, payload);
 #ifndef LOWMEM
@@ -462,7 +461,7 @@ static int nfqueue_cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg, struct nf
             } else {
                 // we set the user-defined accept_mark and set NF_REPEAT verdict
                 // it's up to other iptables rules to decide what to do with this marked packet
-                nfq_set_verdict2(qh, id, NF_REPEAT, accept_mark, 0, NULL);
+                status = nfq_set_verdict_mark(qh, id, NF_REPEAT, accept_mark, 0, NULL);
             }
             break;
         default:
@@ -497,10 +496,10 @@ static int nfqueue_bind() {
 
     do_log(LOG_INFO, "INFO: Binding to queue %hu", queue_num);
     if (accept_mark) {
-        do_log(LOG_INFO, "INFO: ACCEPT mark: %u", accept_mark);
+        do_log(LOG_INFO, "INFO: ACCEPT mark: %u", ntohl(accept_mark));
     }
     if (reject_mark) {
-        do_log(LOG_INFO, "INFO: REJECT mark: %u", reject_mark);
+        do_log(LOG_INFO, "INFO: REJECT mark: %u", ntohl(reject_mark));
     }
     nfqueue_qh = nfq_create_queue(nfqueue_h, queue_num, &nfqueue_cb, NULL);
     if (!nfqueue_qh) {
@@ -634,10 +633,10 @@ int main(int argc, char *argv[]) {
             queue_length = (uint32_t)atoi(optarg);
             break;
         case 'r':
-            reject_mark = (uint32_t)atoi(optarg);
+            reject_mark = htonl((uint32_t)atoi(optarg));
             break;
         case 'a':
-            accept_mark = (uint32_t)atoi(optarg);
+            accept_mark = htonl((uint32_t)atoi(optarg));
             break;
         case 'm':
             opt_merge = 1;